eb9659caf80a2aa2105f15f013651faea4aeb0c1
fmarcos <fmarcosdev@gmail.com>
2021-03-12 21:46:59 -0300

    efecto spinner y 2 posibilidades de busqueda por ahora

--- scripts.js
+++ scripts.js
@@ -1,82 +1,110 @@
-//inputs
-let inputSearch = document.querySelector("#input-search")
-let output  = document.querySelector("#maincontent")
+
+// inputs
+const inputSearch = document.querySelector('#input-search');
+const output = document.querySelector('#maincontent');
+
+// event content loaded on page
+document.addEventListener('DOMContentLoaded', fetchApi());
 
 
-//event content loaded on page
-document.addEventListener("DOMContentLoaded", loading());
+//search for input
+inputSearch.addEventListener("change", ()=>{
+  iconloader();
+  
+  let newSearch = inputSearch.value;   fetchApi(newSearch);
+})
 
-//main function
-async function loading(typequery){
-let res;
 
-if(typequery){
-  // res =  await getProducts("https://bsale-backend.herokuapp.com/api/product"); // get all products
-}else{
-   res =  await getProducts("https://bsale-backend.herokuapp.com/api/product"); // get all products
-}
 
-//
-let qt = res.forEach(character => {
-//new card
-var contentCard = document.createElement("div")
-contentCard.className = "contentCard";
+// main function
+async function fetchApi(matchQuery) {
+  let res;
+  if (matchQuery) {
+    res =  await getProducts("http://localhost:3000/api/product/"+matchQuery); // search a product
+console.log("busqueda personalizada")
+  console.log(res)
+  }   else {
+  res = await getProducts('http://localhost:3000/api/product');  // all products
+  console.log("todos los resultados")
+  console.log(res)
+  }
 
-//the new content to this card
-const htmlstring = `
-<div class="card">
+
+  //clear input to print result
+  output.innerHTML="";
+  res.map((data)=>{
+    const      htmlString  =`
+        <div class="card">
           <div>
-            <p>discount</p>
+            <p>Descuento de ${data.discount}</p>
           </div>
-          <img src="url_image" alt="Avatar" style="width:100%">
+          <img src="${data.url_image}" alt="Avatar" style="width:100%">
           <div class="container">
-            <b>nombre</b>
+            <b>${data.name}</b>
           </div>
           <div class="container">
             <div>
-              <p>price</p>
-            </div>
-            <div>
-              category
+              <p>${data.price}</p>
             </div>
           </div>
         </div>
-`
-// insert content to the card
-contentCard.innerHTML = htmlstring;
-output.appendChild(contentCard) // append
+`;  
+  
+  let outputString = document.createElement('div');
+  outputString.classList.add('contentCard' )
+  outputString.innerHTML = htmlString;
+  output.appendChild(outputString)
   })
 
 
 
+//   res.forEach((data) => {
+//     // new card
+//     let htmlstring
+  
+//     const contentCard = document.createElement('div');
+   
+//     contentCard.className = 'contentCard';
+//      htmlstring = `
+//         <div class="card">
+//           <div>
+//             <p>Descuento de ${data.discount}</p>
+//           </div>
+//           <img src="${data.url_image}" alt="Avatar" style="width:100%">
+//           <div class="container">
+//             <b>${data.name}</b>
+//           </div>
+//           <div class="container">
+//             <div>
+//               <p>${data.price}</p>
+//             </div>
+//           </div>
+//         </div>
+// `;
 
 
-// let qwerty = await res.map(r =>{
-//   htmlString =+ `<p>hola</p>`;
-// })
-
-  // output.appendChild(qwerty)
-
-// console.log(htmlString)
-// console.log(outputString)
-
-
-
+    // // insert content to the card
+    // contentCard.innerHTML = htmlstring;
+    // output.append(contentCard);
+    
+  // });
 }
 
 // function fetch return data json
-async function getProducts(fromUrl){
+async function getProducts(fromUrl) {
   return fetch(fromUrl)
- .then(response => response.json())
-  .then(data => {
-    return (data.list)
-  })
-  .catch(err =>  console.log(err))
+    .then((response) => response.json())
+    .then((data) => {
+      return(data)
+    })
+    .catch((err) => console.log(err));
 }
 
+setInterval(() => {
+  console.log(inputSearch.value);
+}, 3000);
 
-setInterval(function(){ 
-  console.log(inputSearch.value)
- }, 3000);
 
-
+function iconloader(){
+  output.innerHTML=` <div class="loader">Loading...</div>`
+}
